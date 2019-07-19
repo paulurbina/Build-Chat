298944ca68bf896bd9121088a9ff6ff4415a5c9e
usrcdimi <usrcdimi@cibertec.edu.pe>
2019-07-19 13:45:37 -0500

    text message add template

--- public/main.js
+++ public/main.js
@@ -29,7 +29,20 @@
 
         // check for connection
         if(socket != undefined) {
-            console.log('connected socket.io');
-            
+            console.log('>>Connected Socket.io...');
+
+            socket.on('output', (data) => {
+                console.log(data);
+                if(data.lenght) {
+                    for(var x=0; x < data.lenght; x++) {
+                        // build out message div
+                        var message = document.createElement('div');
+                        message.setAttribute('class', 'chat-message');
+                        message.textContent = data[x].name + ": " + data[x].message;
+                        messages.appendChild(message);
+                        messages.insertBefore(message, messages.firstChild);
+                    }
+                }
+            });
         }
 })();
