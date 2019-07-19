e7729901b422c20683bb580fef4fa693de73d199
usrcdimi <usrcdimi@cibertec.edu.pe>
2019-07-19 12:27:35 -0500

    config db

--- server.js
+++ server.js
@@ -1,51 +1,54 @@
 const mongoClient = require('mongodb').MongoClient;
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 const client = require('socket.io').listen(5050).sockets;
-const urlMlab = 'mongodb://chat-socket-paul:buildchat2019@ds253017.mlab.com:53017/build-chat-socket-io';
+const urlMlab = 'mongodb://userchat:buidlchat2019@ds253017.mlab.com:53017/buildchatnode';
+const urlNative = 'mongodb://127.0.0.1/mongochat';
+//suport promise
+// mongoose.Promise = global.Promise;
 
 // Connect  db mongo
-mongo.connect(urlMlab, { useNewUrlParser: true})
-    .then(db => {
-        console.log('>>Connected Mongodb..');
+mongoClient.connect(urlNative, { useNewUrlParser:true }, function(err ,db) {
+    if(err) {
+        throw err;
+    } else {
+        console.log('>> Connected db...');
 
-        // Connected socket.io
         client.on('connection', (socket) => {
             let chat = db.collection('chats');
 
-            //create functo send status
-            let sendStatus = s => {
-                socket.emit('status', s);
+            //create function  send status
+            let sendStatus = (status) => {
+                socket.emit('status', status);
             }
-            // get chats  from mongo collection
+
+            //get chat mongodb from collections
             chat.find().limit(100).sort({ _id: -1 }).toArray((err, res) => {
-                if (err) {
-                    throw err;
+                if(err) {
+                    throw err
                 } else {
-                    
-                    //emit the messages
+                    //emit messages
                     socket.emit('output', res);
                 }
             });
 
-            // handlees inputs events
+            // handles inputs events
             socket.on('input', (data) => {
-                let name = data.name;
-                let message = data.message;
+                let name = data.name || '';
+                let message = data.message || '';
 
-                //check for name and message
+                //check message and name
                 if(name == '' || message == '') {
-                    // send error status
-                    sendStatus('Please enter message and name');
+                    sendStatus('Please enter name and message');
                 } else {
-                    //insert messsage
+                    // insert messages
                     chat.insert({
                         name: name,
                         message: message
                     }, () => {
                         client.emit('output', [data]);
 
-                        //send status object
+                        // send status object
                         sendStatus({
                             message: 'Message send',
                             clear: true
@@ -54,19 +57,15 @@ mongo.connect(urlMlab, { useNewUrlParser: true})
                 }
             });
 
-            // handle clear
+            //handle clear
             socket.on('clear', (data) => {
                 // remove all chat collection
                 chat.remove({}, () => {
-                    //emit cleared
+                    //emit chat remove
                     socket.emit('cleared');
                 });
             });
-
         });
+    }
+});
 
-    })
-    .catch(err => {
-        console.log(err);
-        throw err;
-    });
